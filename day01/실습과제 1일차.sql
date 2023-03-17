b2bd5fdc46c5beb3110adac3073743209130997e
David Byun <byundavid@naver.com>
2023-03-17 13:52:19 +0900

    0317 sql 과제 제출

--- day01/실습과제 1일차.sql
+++ day01/실습과제 1일차.sql
@@ -1,33 +1,3 @@
-<학습 평가 과제(1일차)>
-
-Q1-1
-(1)  논리 설계
-(2)  데이터 모델링
-
-Q1-2
-(1) E-R 모델
-(2) E-R 모델
-(3) Entity
-
-Q1-3
-(1) E-R Diagram
-(2) 관계
-
-Q1-4
-(1) 카디널리티(Cardinality)
-(2) 옵셔널리티(Optionality)
-
-Q1-5
-(1) 스키마
-(2) 테이블
-
-Q1-6
-(1) Table
-(2) Index
-(3) Sequence
-
-<실습 평가 과제(1일차)>
-
 /* 실습과제 139P */
 SELECT EMPNO AS EMPLOYEE_NO
         , ENAME AS EMPLOYEE_NAME
@@ -76,7 +46,7 @@ WHERE DEPTNO = 30
 SELECT *
 FROM EMP
 WHERE JOB IN ('MANAGER', 'CLERK')
-AND ENAME NOT LIKE ('_S%')
+AND ENAME NOT LIKE ('_L%')
 AND EMPNO IN
 (SELECT EMPNO FROM EMP
 WHERE COMM IS NULL

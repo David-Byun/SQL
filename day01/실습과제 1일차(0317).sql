7b64c63809a48dd9cc8789a3dba2563ca180a4a5
David Byun <byundavid@naver.com>
2023-03-17 13:38:03 +0900

    assignment branch 세팅

---
+++ day01/실습과제 1일차(0317).sql
@@ -0,0 +1,143 @@
+<학습 평가 과제(1일차)>
+
+Q1-1
+(1)  논리 설계
+(2)  데이터 모델링
+
+Q1-2
+(1) E-R 모델
+(2) E-R 모델
+(3) Entity
+
+Q1-3
+(1) E-R Diagram
+(2) 관계
+
+Q1-4
+(1) 카디널리티(Cardinality)
+(2) 옵셔널리티(Optionality)
+
+Q1-5
+(1) 스키마
+(2) 테이블
+
+Q1-6
+(1) Table
+(2) Index
+(3) Sequence
+
+<실습 평가 과제(1일차)>
+
+/* 실습과제 139P */
+SELECT EMPNO AS EMPLOYEE_NO
+        , ENAME AS EMPLOYEE_NAME
+        , JOB
+        , MGR AS MANAGER
+        , HIREDATE
+        , SAL AS SALARY
+        , COMM AS COMMISSION
+        , DEPTNO AS DEPARTMENT_NO
+FROM EMP
+ORDER BY DEPTNO DESC, ENAME ASC
+
+/* 실습과제 146P */
+SELECT * FROM EMP
+        WHERE COMM IN NULL AND SAL > NULL;
+SELECT * FROM EMP
+        WHERE MGR IS NULL AND COMM IS NULL;
+
+/* 실습과제 150P 1번 */
+SELECT *
+FROM EMP
+WHERE ENAME LIKE ('%S')
+
+/* 실습과제 150P 2번 */
+SELECT *
+FROM EMP
+WHERE JOB = 'SALESMAN' AND DEPTNO = 30
+
+/* 실습과제 150P 3번 */
+SELECT *
+FROM EMP
+WHERE DEPTNO IN (20, 30)
+AND SAL > 2000
+
+/* 실습과제 150P 4번 */
+SELECT *
+FROM EMP
+WHERE SAL > 2000
+AND DEPTNO = 20
+UNION
+SELECT *
+FROM EMP
+WHERE DEPTNO = 30
+
+/* 실습과제 150P 5번 */
+SELECT *
+FROM EMP
+WHERE JOB IN ('MANAGER', 'CLERK')
+AND ENAME NOT LIKE ('_S%')
+AND EMPNO IN
+(SELECT EMPNO FROM EMP
+WHERE COMM IS NULL
+AND MGR IS NOT NULL)
+
+/* 실습과제 169P 1번 */
+SELECT EMPNO, ENAME
+    , SUBSTR(EMPNO, 1, 2) || LPAD('*', LENGTH(EMPNO)-2, '*') AS EMPNO_마스킹_처리
+    , SUBSTR(ENAME, 1, 1) || LPAD('*', LENGTH(ENAME)-1, '*') AS ENAME_마스킹_처리
+FROM EMP
+WHERE LENGTH(ENAME) >= 6
+
+/* 실습과제 169P 2번 */
+SELECT EMPNO, ENAME,
+       JOB,
+       SAL,
+       SAL / 20 AS DAY_PER_SAL,
+       SAL / 20 / 8 AS HOUR_PER_SAL
+FROM EMP
+WHERE JOB IN ('SALESMAN', 'CLERK')
+
+/* 실습과제 170P 3번*/
+SELECT EMPNO, ENAME, HIREDATE,
+    TO_CHAR(NEXT_DAY(ADD_MONTHS(HIREDATE, 3), '월요일'), 'YYYY-MM-DD') AS R_JOB,
+    NVL(TO_CHAR(COMM), 'N/A') AS COMM
+FROM EMP
+
+
+/* 실습과제 170P 4번*/
+SELECT EMPNO, ENAME, MGR
+    , CASE
+        WHEN MGR IS NULL THEN '0000'
+        WHEN SUBSTR(MGR,1,2) = '75' THEN '5555'
+        WHEN SUBSTR(MGR,1,2) = '76' THEN '6666'
+        WHEN SUBSTR(MGR,1,2) = '77' THEN '7777'
+        WHEN SUBSTR(MGR,1,2) = '78' THEN '8888'
+        ELSE '9999'
+        END AS CHG_MGR
+FROM EMP
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
